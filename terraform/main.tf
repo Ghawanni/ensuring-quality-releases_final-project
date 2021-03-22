588339cc6cbcfe201993851a70bab1114141e882
Mohammed Ghawanni <m7md.ghawanni@gmail.com>
2021-03-22 22:19:02 +0300

    changed access key to be an inline var

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@ terraform {
     storage_account_name = "tstate5776"
     container_name       = "tstate"
     key                  = "terraform.tfstate"
-    access_key           = "${var.storage_account_access_key}"
+    access_key           = "gGzngUNcH5nxljnR/VK8ppslDojXsbbeQiOJvB9vMlcGdli2fA0CFrrytPgQp3JONdm5FYhxmjyDo7DJugxz/w=="
   }
 }
 module "resource_group" {
